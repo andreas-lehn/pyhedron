dc1b5198eaaea80da49bf7dacd954b7fd056835c
Andreas Lehn <andreas.lehn@icloud.com>
2023-11-24 21:34:47 +0100

    First package created.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[ 'numpy' ],
 
     classifiers=[
-        'Development Status :: Experimental',
+        'Development Status :: 2 - Pre-Alpha',
         'License :: OSI Approved :: MIT License',  
         'Programming Language :: Python :: 3',
     ],
